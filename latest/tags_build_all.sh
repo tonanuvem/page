a1ec1a12ee7b76ae2aeb4cc57f9ee19bafa700a9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-10 16:38:37 -0300

    Update tags_build_all.sh

--- latest/tags_build_all.sh
+++ latest/tags_build_all.sh
@@ -1,6 +1,6 @@
 # Gera a imagem com as diversas tags com base nos diferentes Dockerfiles:
 echo 'Removendo imagens antigas do ambiente:'
-docker rmi $(docker images -a -q | grep 'tonanuvem/fiap_page')
+docker rmi $(docker images tonanuvem/fiap_page* -q)
 echo 'Iniciando os builds:'
 cd ../blue && echo ' fazendo o build em' $(pwd) && sh build.sh
 cd ../latest && echo ' fazendo o build em' $(pwd) && sh build.sh
