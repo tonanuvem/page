319013ef20e772b1f856c56fccd947dfa0f3439b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-10 17:03:16 -0300

    Create push_all_repo.sh

---
+++ latest/push_all_repo.sh
@@ -0,0 +1,6 @@
+docker login
+docker push tonanuvem/fiap_page:green
+docker push tonanuvem/fiap_page:blue
+docker push tonanuvem/fiap_page:latest
+docker push tonanuvem/fiap_page:teams
+docker push tonanuvem/fiap_page:slack
