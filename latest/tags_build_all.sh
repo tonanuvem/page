2a0c4f0308384f8f4d07971339ad84899227ba35
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-05-10 16:28:00 -0300

    Update and rename tags_build_all.sh to tags_build_all.sh

--- latest/tags_build_all.sh
+++ latest/tags_build_all.sh
@@ -1,7 +1,7 @@
 # Gera a imagem com as diversas tags com base nos diferentes Dockerfiles:
 echo 'Iniciando os builds:'
 cd ../blue && echo ' fazendo o build em' $(pwd) && sh build.sh
-cd ../compose && echo ' fazendo o build em' $(pwd) && sh build.sh
+cd ../latest && echo ' fazendo o build em' $(pwd) && sh build.sh
 cd ../slack && echo ' fazendo o build em' $(pwd) && sh build.sh
 cd ../teams && echo ' fazendo o build em' $(pwd) && sh build.sh
 docker images
